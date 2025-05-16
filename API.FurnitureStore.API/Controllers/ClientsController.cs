6eb11f0d3ecc81bd022913df5fe0fbb2030cd94e
Sergio Suárez <62405866+sergio200086@users.noreply.github.com>
2025-05-16 16:43:40 -0500

    Update .gitignore

---
+++ API.FurnitureStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,38 @@
+﻿using API.FurnitureStore.Data;
+using API.FurnitureStore.Share;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApiFurnitureStoreContext _context;
+
+        public ClientsController(ApiFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+            //return al clients listed in the DbContext
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.id == id);
+            
+            if (client == null) 
+                return NotFound($"Client with ID {id} was not found");
+
+            return Ok(client);
+        }
+    }
+}
